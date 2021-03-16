2ca8833928c1198c8adc413ef7e8d6bf6b9b0a07
Kieren Foenander <kieren.foenander@cqumail.com>
2021-03-16 20:44:31 +1000

    added customer class

--- src/Assignment1/Customer.java
+++ src/Assignment1/Customer.java
@@ -1,16 +1,92 @@
 /*
  * /*
 Programmer: Kieren Foenander S012141776
-Course: Object Oriented Programming COIT11134
+Course: Applied Distributed Systems
 Assignment 1
  */
 
 package Assignment1;
 
-/**
- *
- * @author black
- */
-public class Customer {
 
+public class Customer {
+    
+    // instance variables of a customer
+    
+    private String clientId;
+    private int pinNumber;
+    private boolean status;
+    private int numberOfTravels;
+    private double totalCost;
+    
+    // default constructor
+    Customer(){
+        
+    }
+    
+    //parameterised constructor
+    Customer(String clientId, int pinNumber, boolean status, int numberOfTravels, double totalCost){
+        
+        this.clientId = clientId;
+        this.pinNumber = pinNumber;
+        this.status = status;
+        this.numberOfTravels = numberOfTravels;
+        this.totalCost = totalCost;
+        
+        
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getPinNumber() {
+        return pinNumber;
+    }
+
+    public void setPinNumber(int pinNumber) {
+        this.pinNumber = pinNumber;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getNumberOfTravels() {
+        return numberOfTravels;
+    }
+
+    public void setNumberOfTravels(int numberOfTravels) {
+        this.numberOfTravels = numberOfTravels;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    
+    public void calculateCost(){
+        /* TO DO method is used to calculate the cost of the trip */
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + "clientId=" + clientId + ", pinNumber=" + pinNumber + ", status=" + status + ", numberOfTravels=" + numberOfTravels + ", totalCost=" + totalCost + '}';
+    }
+    
+
+    
+    
+    
+    
+    
+    
+     
 }
