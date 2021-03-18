41be22dfb23f33d7a258855b4e3d244e714e2ed4
Kieren Foenander <kieren.foenander@cqumail.com>
2021-03-18 22:45:03 +1000

    started client class

--- src/Assignment1/UDPClient.java
+++ src/Assignment1/UDPClient.java
@@ -7,6 +7,13 @@ Assignment 1
 
 package Assignment1;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
 
 public class UDPClient {
     
@@ -16,9 +23,50 @@ public class UDPClient {
     
     public static void main (String [] args){
         
+        DatagramSocket socketA = null;
+        try{
+            
+            socketA = new DatagramSocket(); // create udp socket
+            
+            Scanner scanner = new Scanner(System.in); // creates new scanner
+            menuResponse();
+            String msg = scanner.nextLine();
+            // prepare message to send to server
+            byte [] bytes = msg.getBytes();
+            InetAddress aHost = InetAddress.getByName(serverName);
+            
+            //create a UDP datagram
+            DatagramPacket request = new DatagramPacket(bytes, msg.length(), aHost, serverPort);
+            //send the request
+            socketA.send(request);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+        catch(SocketException e){
+            
+        }catch(IOException e){
+        
+        }
+        finally{
+            if (socketA != null) socketA.close();
+        }
         
         
         
+        
+    }
+    
+    
+    public static void menuResponse(){
+        System.out.println("****** Travel Kiosk ******\n         1: IN\n         2: OUT\n         3: EXIT");
+
     }
 
 }
