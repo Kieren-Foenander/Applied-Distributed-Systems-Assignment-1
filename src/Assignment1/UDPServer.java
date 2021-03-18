fac5f8301bc8aa626bbb90400d6808ac9814abea
Kieren Foenander <kieren.foenander@cqumail.com>
2021-03-18 21:46:29 +1000

    added write to file functionality and timer to save every 2 minutes

--- src/Assignment1/UDPServer.java
+++ src/Assignment1/UDPServer.java
@@ -10,8 +10,12 @@ package Assignment1;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Timer;
 
 
 public class UDPServer {
@@ -19,6 +23,7 @@ public class UDPServer {
     public static int serverPort = 6789;
     public static String textFileName = "members.txt";
     public static ArrayList <Customer> customerList = new ArrayList();
+    public static long interval = 120000;
     
     
     public static void readCustomers() throws FileNotFoundException, IOException{
@@ -34,7 +39,7 @@ public class UDPServer {
             
             while (lineInput.hasNext()){ // loops through each line to separate information
                 String clientId = lineInput.next(); // gets client id from line
-                int pinNumber = lineInput.nextInt(); // gets piin number from line
+                int pinNumber = lineInput.nextInt(); // gets pin number from line
                 
                 
                 Customer custInput = new Customer(); // creates new customer object
@@ -53,7 +58,40 @@ public class UDPServer {
     public static void main (String [] args) throws FileNotFoundException, IOException{
         
         
-        readCustomers();
+        readCustomers(); // loads customer text file
+        
+        Timer timer = new Timer(); // creates new timer
+        WriteToFile wtf = new WriteToFile(); // creates instance of WriteToFile class
+        
+        timer.schedule(wtf, interval, interval); // schedules a write to file action after 2 minutes of the program running and every 2 minutes after
+        
+        
+        
+        
+        DatagramSocket socketA = null;
+        try{
+            socketA = new DatagramSocket(serverPort);
+            /*
+            (byte[] buffer = new byte[1000];
+            System.out.println("server started");
+            
+            while (true){
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                socketA.receive(request);
+            }
+*/
+        }catch(SocketException e){
+            System.out.println("Socket: " + e.getMessage());
+                    
+        }catch(IOException e){
+            System.out.println("IO: " + e.getMessage());
+        }finally {
+            if (socketA != null){
+                socketA.close();
+            }
+        }
+        
+        
         
         System.out.println(customerList);
  
--- src/Assignment1/WriteToFile.java
+++ src/Assignment1/WriteToFile.java
@@ -7,14 +7,39 @@ Assignment 1
 
 package Assignment1;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.TimerTask;
 
 
 public class WriteToFile extends TimerTask {
     
+    private FileOutputStream fos = null;
+    private ObjectOutputStream oos = null;
+    private String fileName = "members.dat";
+    
+    UDPServer server = new UDPServer();
+    
+    
+    
+    
     @Override
     public void run(){
         
+        try{
+            fos = new FileOutputStream(fileName); // creates new file stream 
+            oos = new ObjectOutputStream(fos);// creates new object output stream
+            
+            for (int i = 0; i < server.customerList.size(); i++){ // loop to add each customer to the text file
+                oos.writeObject(server.customerList.get(i));
+            }
+            oos.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+            
+        }
     }
 
 }
