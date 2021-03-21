d81bf32925df4f11d1f851cfa74980f058601a4e
Kieren Foenander <kieren.foenander@cqumail.com>
2021-03-21 15:41:01 +1000

    lots of trial and error good start to commands

--- src/Assignment1/UDPClient.java
+++ src/Assignment1/UDPClient.java
@@ -18,35 +18,48 @@ import java.util.Scanner;
 public class UDPClient {
     
     public static int serverPort = 6789;
-    public static String serverName = "serverOne";
+    //public static String serverName = "serverOne";
     
     
     public static void main (String [] args){
         
+        menuResponse();
         DatagramSocket socketA = null;
+        
         try{
-            
-            socketA = new DatagramSocket(); // create udp socket
-            
-            Scanner scanner = new Scanner(System.in); // creates new scanner
-            menuResponse();
-            String msg = scanner.nextLine();
-            // prepare message to send to server
-            byte [] bytes = msg.getBytes();
-            InetAddress aHost = InetAddress.getByName(serverName);
-            
-            //create a UDP datagram
-            DatagramPacket request = new DatagramPacket(bytes, msg.length(), aHost, serverPort);
-            //send the request
-            socketA.send(request);
-            
-            
-            
-            
-            
-            
-            
-            
+            while (true) {
+
+                socketA = new DatagramSocket(); // create udp socket
+
+                Scanner scanner = new Scanner(System.in); // creates new scanner
+                
+                String msg = scanner.nextLine();
+
+                // prepare message to send to server
+                byte[] bytes = msg.getBytes();
+                InetAddress aHost = InetAddress.getLocalHost();
+
+                //while(true){
+                //create a UDP datagram
+                DatagramPacket request = new DatagramPacket(bytes, msg.length(), aHost, serverPort);
+                //send the request
+                socketA.send(request);
+
+                byte[] buffer = new byte[1000]; // prepare a buffer to recieve the reply from the server
+
+                //waiting for reply
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+                socketA.receive(reply);
+                
+                if (new String (reply.getData(), 0, reply.getLength()).equalsIgnoreCase("exit")){
+                    System.out.println("exit System");
+                    break;
+                }
+
+                System.out.println("server response: " + new String(reply.getData(), 0, reply.getLength()));
+
+            }
+            socketA.close();
             
         }
         catch(SocketException e){
@@ -55,7 +68,7 @@ public class UDPClient {
         
         }
         finally{
-            if (socketA != null) socketA.close();
+           if (socketA != null) socketA.close();
         }
         
         
@@ -65,7 +78,7 @@ public class UDPClient {
     
     
     public static void menuResponse(){
-        System.out.println("****** Travel Kiosk ******\n         1: IN\n         2: OUT\n         3: EXIT");
+        System.out.println("****** Travel Kiosk ******\n         1: IN\n         2: OUT\n         3: EXIT\nEnter: ");
 
     }
 
--- src/Assignment1/UDPServer.java
+++ src/Assignment1/UDPServer.java
@@ -4,7 +4,6 @@ Programmer: Kieren Foenander S012141776
 Course: Applied Distributed Systems
 Assignment 1
  */
-
 package Assignment1;
 
 import java.io.FileNotFoundException;
@@ -12,88 +11,146 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Timer;
 
-
 public class UDPServer {
-    
+
     public static int serverPort = 6789;
     public static String textFileName = "members.txt";
-    public static ArrayList <Customer> customerList = new ArrayList();
+    public static ArrayList<Customer> customerList = new ArrayList();
     public static long interval = 120000;
-    
-    
-    public static void readCustomers() throws FileNotFoundException, IOException{
-        
-        
-        
+
+    public static void readCustomers() throws FileNotFoundException, IOException {
+
         FileReader reader = new FileReader(textFileName); // creates a new file reader to read members.txt
         Scanner input = new Scanner(reader); // creates new scanner for file
-        
-        while (input.hasNextLine()){ // loops through text file to get info
+
+        while (input.hasNextLine()) { // loops through text file to get info
             String line = input.nextLine(); // creates string with each line in document
             Scanner lineInput = new Scanner(line); // creates new scanner to scan each line to extract information further
-            
-            while (lineInput.hasNext()){ // loops through each line to separate information
+
+            while (lineInput.hasNext()) { // loops through each line to separate information
                 String clientId = lineInput.next(); // gets client id from line
                 int pinNumber = lineInput.nextInt(); // gets pin number from line
-                
-                
+
                 Customer custInput = new Customer(); // creates new customer object
                 custInput.setClientId(clientId); // adds client id to object
                 custInput.setPinNumber(pinNumber); // adds pin number to object
-                
+
                 customerList.add(custInput); // adds new customer object to arraylist
-                
+
             }
         }
         reader.close();
     }
-    
-    
-    
-    public static void main (String [] args) throws FileNotFoundException, IOException{
-        
-        
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
         readCustomers(); // loads customer text file
-        
+        String response = "";
+        String user = "";
+
         Timer timer = new Timer(); // creates new timer
         WriteToFile wtf = new WriteToFile(); // creates instance of WriteToFile class
-        
+
         timer.schedule(wtf, interval, interval); // schedules a write to file action after 2 minutes of the program running and every 2 minutes after
-        
-        
-        
-        
+
         DatagramSocket socketA = null;
-        try{
+        try {
             socketA = new DatagramSocket(serverPort);
-            /*
-            (byte[] buffer = new byte[1000];
+
+            byte[] buffer = new byte[1000];
             System.out.println("server started");
-            
-            while (true){
+
+            while (true) {
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 socketA.receive(request);
-            }
-*/
-        }catch(SocketException e){
-            System.out.println("Socket: " + e.getMessage());
+                String str = new String(request.getData(), 0, request.getLength());
+                System.out.println("Client Request: " + new String(request.getData(), 0, request.getLength()));
+               
+                if (str.equalsIgnoreCase("1")){
+                    response = "Enter Client Id";
                     
-        }catch(IOException e){
+                }else if (str.equalsIgnoreCase("2")){
+                    response = "out";
+                    
+                }else if(str.equalsIgnoreCase("3")){
+                    response = "exit";
+                }
+                for (int i = 0; i < customerList.size(); i++){
+                        if (str.equalsIgnoreCase(customerList.get(i).getClientId())){
+                            response = "user found";
+                            
+                        }
+                }
+                
+                    
+                    byte[] b = response.getBytes();
+                    DatagramPacket reply = new DatagramPacket(b, response.length(), request.getAddress(), request.getPort());
+                    socketA.send(reply);
+                    
+                    Arrays.fill(buffer, (byte)0);
+                /* if (str.equalsIgnoreCase("1")) {
+                    
+                    
+                    DatagramPacket request1 = new DatagramPacket(buffer, buffer.length);
+                    socketA.receive(request1);
+                    String clientId = new String(request1.getData(), 0, request1.getLength());
+                    System.out.println("Client Request: " + new String(request1.getData(), 0, request1.getLength()));
+                    
+                    for (int i = 0; i < customerList.size(); i++){
+                        if (clientId.equalsIgnoreCase(customerList.get(i).getClientId()) );
+                        String idResponse = "enter client pin";
+                        b = idResponse.getBytes();
+                        DatagramPacket idReply = new DatagramPacket(b,idResponse.length(), request.getAddress(), request.getPort());
+                        socketA.send(idReply);
+                        
+                    }
+                    
+                    /*DatagramPacket request1 = new DatagramPacket(buffer, buffer.length);
+                    socketA.receive(request1);
+                    String rqst = new String(request.getData(), 0, request.getLength());
+                    System.out.println("Client Request 2: " + new String(request.getData(), 0, request.getLength()));
+
+                }else if (str.equalsIgnoreCase("2")){
+                    String response = "Enter bitch ID: ";
+                    byte[] b = response.getBytes();
+                    DatagramPacket reply = new DatagramPacket(b, response.length(), request.getAddress(), request.getPort());
+                    socketA.send(reply);
+                }
+*/
+            }
+
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+
+        } catch (IOException e) {
             System.out.println("IO: " + e.getMessage());
-        }finally {
-            if (socketA != null){
+        } finally {
+            if (socketA != null) {
                 socketA.close();
             }
         }
-        
-        
-        
+
         System.out.println(customerList);
- 
+
+    }
+
+    public static void signIn(String userId, int pinNumber){
+
+        for (int i = 0; i < customerList.size(); i++){
+            if (userId.equalsIgnoreCase(customerList.get(i).getClientId()) && pinNumber ==(customerList.get(i).getPinNumber()));
+            
+            customerList.get(i).setStatus(true);
+        }
+        
+        
+
     }
 }
